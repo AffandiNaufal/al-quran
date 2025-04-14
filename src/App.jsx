f9c17af5487887289c705231019a90b80c3331b2
AffandiNaufal <affandinaufal03@gmail.com>
2025-04-14 15:18:01 +0700

    Siap Publish

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<Dashboard />}></Route>
                 <Route path="/surat/:id" element={<DetailSurat />}></Route>
+                <Route path="/" element={<About />}></Route>
               </Routes>
             </Content>
           </div>
