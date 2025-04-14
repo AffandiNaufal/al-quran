6b8cb92e0603c93e3c1912ccbf93342385118c3f
AffandiNaufal <affandinaufal03@gmail.com>
2025-04-14 15:03:12 +0700

    Siap Publish

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+        <h4>Biodata Diri</h4>
+        <p>Nama : Naufal Affandi</p>
+        <p>NIM : 12350112959</p>
+    </>
+  )
+}
+
+export default About
