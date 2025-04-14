38e78a487e50a876d3892b7af729c98771cf7c12
AffandiNaufal <affandinaufal03@gmail.com>
2025-04-14 15:31:20 +0700

    siap publish

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import "./App.css";
 import Navbar from "./components/Navbar";
 import Content from "./components/Content";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import DetailSurat from "./pages/DetailSurat";
 import About from "./pages/About";
